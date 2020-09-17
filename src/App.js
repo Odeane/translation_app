ac52f9be8e47afaeb801f9f44d988b1d55560de2
odeane_croney <odeane_croney@yahoo.com>
2020-09-17 11:30:21 +0200

    translation iplimentation code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Field from "./components/field";
+import Languages from "./components/languages";
+import Translate from "./components/translate";
 
-function App() {
+export default function App() {
+  const [language, setLanguage] = useState("ru");
+  const [text, setText] = useState("");
+
   return (
-    <div className="App">
-      
+    <div>
+      <Field label="Enter English" onChange={setText} value={text} />
+      <Languages language={language} onLanguageChange={setLanguage} />
+      <hr />
+      <Translate text={text} language={language} />
     </div>
   );
 }
-
-export default App;
